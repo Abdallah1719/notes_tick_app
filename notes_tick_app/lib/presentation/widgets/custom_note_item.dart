784f59ac3755d2080f8422bf68353ffa5a32aa46
abdallah1719 <abdallahtolba17@gmail.com>
2024-06-05 01:51:24 +0300

    create custom note item

---
+++ notes_tick_app/lib/presentation/widgets/custom_note_item.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class CustomNoteItem extends StatelessWidget {
+  const CustomNoteItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: const EdgeInsets.only(top: 16, bottom: 16, left: 16),
+        decoration: BoxDecoration(
+          color: const Color(0xffffcc80),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: const Text(
+                'Flutter tips',
+                style: TextStyle(color: Colors.black, fontSize: 26),
+              ),
+              subtitle: const Padding(
+                padding: EdgeInsets.only(top: 16, bottom: 16),
+                child: Text('bulid your carerr with abdallah tolba ',
+                    style: TextStyle(color: Colors.black38, fontSize: 18)),
+              ),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.delete),
+                color: Colors.black,
+                iconSize: 24,
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(right: 30),
+              child:
+                  Text('21May,2024', style: TextStyle(color: Colors.black38)),
+            )
+          ],
+        ),
+      );
+  }
+}
