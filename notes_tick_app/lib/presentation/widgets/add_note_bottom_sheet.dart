d301fdc593773f7ebb88b5d5c24a7e129383dd32
abdallah1719 <abdallahtolba17@gmail.com>
2024-06-05 03:16:08 +0300

    add note bottom sheet

---
+++ notes_tick_app/lib/presentation/widgets/add_note_bottom_sheet.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AddNoteBottomSheet extends StatelessWidget {
+  const AddNoteBottomSheet({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // child: const Padding(
+      //   padding: EdgeInsets.symmetric(horizontal: 16),
+      //   child: SingleChildScrollView(
+      //     child: AddNoteForm(),
+      //   ),
+      // ),
+    );
+  }
+}
--- notes_tick_app/lib/presentation/widgets/notes_list_view.dart
+++ notes_tick_app/lib/presentation/widgets/notes_list_view.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 import 'custom_note_item.dart';
@@ -8,13 +7,16 @@ class NotesListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-        padding: EdgeInsets.zero,
-        itemBuilder: (context, index) {
-          return const Padding(
-            padding: EdgeInsets.symmetric(vertical: 4.0),
-            child: NoteItem(),
-          );
-        });
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16),
+      child: ListView.builder(
+          padding: EdgeInsets.zero,
+          itemBuilder: (context, index) {
+            return const Padding(
+              padding: EdgeInsets.symmetric(vertical: 4.0),
+              child: NoteItem(),
+            );
+          }),
+    );
   }
 }
